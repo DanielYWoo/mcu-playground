4c956c11d2a84b0136929ad50f4cccb32addebd2
Daniel Wu <daniel.y.woo@gmail.com>
2020-01-19 20:08:54 +0800

    ritchie watering

--- arduino-watering-1/arduino-watering-1.ino
+++ arduino-watering-1/arduino-watering-1.ino
@@ -14,15 +14,31 @@ const int PIN_1602_D7 = 9;
 LiquidCrystal lcd(PIN_1602_RS, PIN_1602_EN, PIN_1602_D4, PIN_1602_D5, PIN_1602_D6, PIN_1602_D7); // create an LCD object
 
 // -------------- controls vaiables -----------------
-
-
-
+int m01 = 10;
+bool refill = false;
+long t1=0;
 void setup() {
-  Serial.begin(9600);  
+  Serial.begin(9600);
   lcd.begin(16, 2);
-  lcd.clear();  
+  lcd.clear();
 }
 
 void loop() {
-  
+  m01 = analogRead(A0);
+  lcd.print("F01 M=");
+  lcd.print(m01);
+  lcd.setCursor(0, 1);
+  lcd.print("refill=");
+  lcd.print(refill);
+  if (m01 > 500 && millis() - t1 > 18000) {
+    Serial.println("watering");
+    delay(5000);
+    t1 = millis();
+    Serial.println("done");
+  }
+
+  delay(1000);
+  lcd.clear();
+
+
 }
