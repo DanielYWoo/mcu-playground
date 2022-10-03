43eeef76da861db3fad9fdcde78fb80f355aac31
Daniel Wu <daniel.wu06@sap.com>
2022-10-03 23:45:23 +0800

    readme

--- arduino-zhamao/arduino-zhamao.ino
+++ arduino-zhamao/arduino-zhamao.ino
@@ -1,7 +1,6 @@
 #include <Otto9.h>
 #include <SoftwareSerial.h>
 
-
 #define PIN_YL 2 //servo[0]  left leg
 #define PIN_YR 3 //servo[1]  right leg
 #define PIN_RL 4 //servo[2]  left foot
@@ -12,23 +11,12 @@
 #define PIN_SOFT_RX  10
 #define PIN_SOFT_TX  11
 #define PIN_BT_STATUS  12
+#define MOVE_SPEED 800
 
-
+const bool beep = false;
 Otto9 zhamao;
 SoftwareSerial BTSerial(PIN_SOFT_RX, PIN_SOFT_TX); // Arduino RX|TX, HC05 TX|RX
 
-#define CMD_HELP '?'
-#define CMD_STAND 's'
-#define CMD_WALK_FORWARD 65
-#define CMD_WALK_BACKWARD 66
-#define CMD_WALK_LEFT 68
-#define CMD_WALK_RIGHT 67
-#define MOVE_SPEED 800
-
-/*
-
-byte buf[32];
-int bufIndex = 0;
 unsigned long lastHeartbeat = millis();
 
 void ensureConnected(boolean forceConnect) {
@@ -40,43 +28,16 @@ void ensureConnected(boolean forceConnect) {
   if (!forceConnect && digitalRead(PIN_BT_STATUS) == HIGH) {
     return;
   }
-  
-  zhamao.sing(S_mode3); //disconnected  
+  if (beep) zhamao.sing(S_mode3); //disconnected  
   while (digitalRead(PIN_BT_STATUS) == LOW) {
     Serial.println("connecting bluetooth...");
     delay(1000);
   }
   
-  BTSerial.begin(38400);
-  zhamao.sing(S_mode1); //connected
+  BTSerial.begin(9600);
+  if (beep) zhamao.sing(S_mode1); //connected
   Serial.println("connected.");
   BTSerial.println("Hello, I am here.");
-}
-
-bool receiveCommand() {
-  delay(500);
-  BTSerial.print('.');
-  Serial.print('.');
-  byte b = BTSerial.read();
-  if (b == 255) {
-    return false; // no data
-  }
-  Serial.print("incoming byte:");
-  Serial.println(b);
-  if (bufIndex >= 31) {
-    BTSerial.println("Error: Command too long");
-    Serial.println("Error: Command too long");
-    return true;
-  }  
-  buf[bufIndex++] = b;
-  return true;
-}
-
-void resetCommand() {
-  bufIndex = 0;
-  for (int i = 0; i < 32; i++) {
-    buf[i] = 0;
-  }
 }
 
 void echo(const String& str) {  
@@ -98,7 +59,7 @@ void setup()
   Serial.println("v3");
 
   zhamao.init(PIN_YL, PIN_YR, PIN_RL, PIN_RR, true, A6, PIN_Buzzer, PIN_Trigger, PIN_Echo); //Set the servo pins and ultrasonic pins and Buzzer pin
-  zhamao.sing(S_connection); //Otto wake up!
+  if (beep) zhamao.sing(S_connection); //Otto wake up!
   zhamao.home();
   delay(50);
 
@@ -111,68 +72,48 @@ void loop()
   ensureConnected(false);
   
   //debugDistance();
-  
-  if (!receiveCommand()) {
-    return;
+  byte cmd = BTSerial.read();
+  if (cmd == 255) {
+    return; // no data
   }
+  Serial.print("incoming byte:");
+  Serial.println(cmd);
+
   
-  if (buf[0] == CMD_HELP) {
-    echo("Welcome, ? -> help, s -> stand, arrow -> move");    
-    resetCommand();
-  } else if (buf[0] == CMD_STAND) {    
-    echo("<STAND");
-    zhamao.home();
-    resetCommand();    
-  } else if (buf[0] == 27) { // escape
-    if (buf[1] == 0) { // buf[1] will come later, return without reset
-      return; 
-    } else if (buf[1] == 91) { // buf[1] arrives
-      if (buf[2] == 0) { // buf[2] will come later, return without reset
-        return;
-      }
-      switch (buf[2]) {
-        case CMD_WALK_FORWARD:
-          echo("<FWD");
-          zhamao.walk(4, MOVE_SPEED, 1); //4 steps, "TIME". IF HIGHER THE VALUE THEN SLOWER (from 600 to 1400), 1 FORWARD          
-          break;
-        case CMD_WALK_BACKWARD:
-          echo("<BACK");
-          zhamao.walk(4, MOVE_SPEED, -1); //4 steps, "TIME". IF HIGHER THE VALUE THEN SLOWER (from 600 to 1400), -1 BACKWARD
-          break;
-        case CMD_WALK_LEFT:          
-          echo("<LEFT");
-          zhamao.turn(3, MOVE_SPEED, 1); //3 steps turning LEFT
-          break;
-        case CMD_WALK_RIGHT:
-          echo("<RIGHT");          
-          zhamao.turn(3, MOVE_SPEED, -1); //3 steps turning RIGHT
-          break;
-        default:
-          echo("unknown");          
-      }
+  switch (cmd) {
+    case '?':
+      echo("Welcome, ? -> help, q -> stand, asdw -> move, e -> roam");
+      break;
+    case 'q':
+      echo(">Stand");
       zhamao.home();
-    } else { // wrong      
-      Serial.print("expect buf[0/1]=27,91, but buf[1] is");
-      Serial.println(buf[1]);
-    }
-  }
-  resetCommand();
-  
-}
-*/
-
-void setup()
-{
-  pinMode(PIN_SOFT_RX, INPUT);
-  pinMode(PIN_SOFT_TX, OUTPUT);
-  Serial.begin(9600);
-  BTSerial.begin(9600);
-  Serial.println("started");
-}
-
-void loop()
-{  
-  BTSerial.write('x');
-  Serial.print('.');
-  delay(1000);
+      break;
+    case 'w':
+      echo(">Move Forward");
+      zhamao.walk(4, MOVE_SPEED, 1); //4 steps, "TIME". IF HIGHER THE VALUE THEN SLOWER (from 600 to 1400), 1 FORWARD
+      zhamao.home();
+      break;
+    case 's':
+      echo(">Move Backward");
+      zhamao.walk(4, MOVE_SPEED, -1); //4 steps, "TIME". IF HIGHER THE VALUE THEN SLOWER (from 600 to 1400), -1 BACKWARD
+      zhamao.home();
+      break;
+    case 'a':
+      echo(">Move Left");
+      zhamao.turn(3, MOVE_SPEED, 1); //3 steps turning LEFT
+      zhamao.home();
+      break;
+    case 'd':
+      echo(">Move Right");          
+      zhamao.turn(3, MOVE_SPEED, -1); //3 steps turning RIGHT
+      zhamao.home();
+      break;
+    case 'e':
+       echo(">Roam");
+       break;
+    default:
+      echo(">Unknown");
+      Serial.print("unexpect command ");
+      Serial.println(cmd);
+  }  
 }
--- arduino-zhamao/readme.md
+++ arduino-zhamao/readme.md
@@ -1,9 +1,28 @@
 # schematic
 
+```
+PIN_YL 2 //servo[0]  left leg
+PIN_YR 3 //servo[1]  right leg
+PIN_RL 4 //servo[2]  left foot
+PIN_RR 5 //servo[3]  right foot
+PIN_Buzzer   6
+PIN_Trigger  8  //TRIGGER pin (8)
+PIN_Echo     9  //ECHO pin (9)
+PIN_SOFT_RX  10  to BT05 TX
+PIN_SOFT_TX  11  to BT05 RX with a voltage divider 5v -> 3.3v
+PIN_BT_STATUS  12 to BT05 STATE
+
+```
 
 
 # bluetooth pairing
+HC05 does not work on the latest MacOS, has to re-pair everytime.
+HC05 does not support BLE so it does not work with iPhone.
+BT05 works with the latest MacOS and iPhone, but it does not work with the terminal with the virtal serial port. It needs something like BlueSee.
+BT05 has a service FFE1, we can write to it, or read data from it.
 
-
+BT05 needs to be set up with a serial USB TTL like CH340G, e.g., set the baud rate to 9600, then in your code you can `BT.begin(9600)`.
 
 # control
+To keep it simple, single byte command.
+
